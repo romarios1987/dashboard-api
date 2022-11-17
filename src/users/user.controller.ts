ca7461eeb80d7677eea1cb7bc89d8f49dfa4c7c0
romarios1987 <romios047@gmail.com>
2022-11-17 18:56:47 +0200

    Auth JWT

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -10,12 +10,16 @@ import { UserRegisterDto } from './dto/user-register.dto';
 import { UserService } from './user.service';
 import { HttpError } from '../errors/http-error.class';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
+import { AuthGuard } from '../common/auth.guard';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
 		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -31,6 +35,12 @@ export class UserController extends BaseController implements IUserController {
 				func: this.login,
 				middlewares: [new ValidateMiddleware(UserLoginDto)],
 			},
+			{
+				path: '/info',
+				method: 'get',
+				func: this.info,
+				middlewares: [new AuthGuard()],
+			},
 		]);
 	}
 
@@ -44,7 +54,9 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HttpError(401, 'Error authorization', 'LOGIN_USER'));
 		}
 
-		this.ok(res, result);
+		const jwt = await this.signJWT(body.email, this.configService.get('SECRET'));
+
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -59,5 +71,31 @@ export class UserController extends BaseController implements IUserController {
 		}
 
 		this.ok(res, result);
+	}
+
+	async info({ user }: Request, res: Response, next: NextFunction): Promise<void> {
+		const userInfo = await this.userService.getUserInfo(user);
+		return this.ok(res, { email: userInfo?.email, id: userInfo?.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
